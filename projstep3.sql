ab749a1ab577b22d11a25c9a7511ec1179acc674
Thomas Gullo <gullot@flip1.engr.oregonstate.edu>
2023-07-28 15:39:30 -0700

    added update and delete functions

--- projstep3.sql
+++ projstep3.sql
@@ -23,3 +23,17 @@ VALUES (:invoiceID, :repairID)
 
 INSERT INTO RepairTypes (repairName, cost)
 VALUES (:repairName, :cost)
+
+-- colon indicates user input. Added update and delete functions
+
+-- update button is included above
+
+--this is what happens when the "SAVE" from the "UPDATE" button is clicked
+UPDATE Customers
+SET customerName = :customerName, telephone = :telephone
+WHERE customerID = :customerID;
+
+--this is the delete button from RepairTypes. Gets rid of a whole row. 
+-- Note that since invoicedetails is delete on cascade, this will delete the associated row in the invoicedetails also
+DELETE FROM RepairTypes WHERE repairID = :repairID;
+
