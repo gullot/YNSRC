6b93725d2185150dc98a9b7ad21a3f5f7ed25c1b
Thomas Gullo <gullot@flip1.engr.oregonstate.edu>
2023-08-02 19:26:26 -0700

    added create (insert) ability to customer table

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+// app.js - SETUP section
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 PORT        = 61287;                 // Set a port number at the top so it's easy to change in the future
 
 // handlebars stuff
@@ -27,7 +31,62 @@ app.get('/', function(req, res)
                                                 // will process this file, before sending the finished HTML to the client.
             })
                           
-    });                                         
+    });    
+// app.js - ROUTES section
+
+app.post('/add-customer-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    /*// Capture NULL values   NO NULL VALUES FOR CUSTOMERS
+    let homeworld = parseInt(data.homeworld);
+    if (isNaN(homeworld))
+    {
+        homeworld = 'NULL'
+    }
+
+    let age = parseInt(data.age);
+    if (isNaN(age))
+    {
+        age = 'NULL'
+    }*/ 
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Customers (customerName, telephone) VALUES  ('${data.customerName}', '${data.telephone}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Customers;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+    
+
 
 /*
     LISTENER
---
+++ public/js/add_customer.js
@@ -0,0 +1,83 @@
+// Get the objects we need to modify
+let addCustomerForm = document.getElementById('add-customer-form-ajax');
+
+// Modify the objects we need
+addCustomerForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputCustomerName = document.getElementById("input-customerName");
+    let inputTelephone = document.getElementById("input-telephone");
+
+    // Get the values from the form fields
+    let customerNameValue = inputCustomerName.value;
+    let telephoneValue = inputTelephone.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        customerName: customerNameValue,
+        telephone: telephoneValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-customer-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputCustomerName.value = '';
+            inputTelephone.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("customer-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 3 cells
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let customerNameCell = document.createElement("TD");
+    let telephoneCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idCell.innerText = newRow.customerID;
+    customerNameCell.innerText = newRow.customerName;
+    telephoneCell.innerText = newRow.telephone;
+
+    // Add the cells to the row 
+    row.appendChild(idCell);
+    row.appendChild(customerNameCell);
+    row.appendChild(telephoneCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
