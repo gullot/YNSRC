665c0351c8878ceb1d482dc30eeb55a7774e79ee
Alexander Dembo <demboa@flip1.engr.oregonstate.edu>
2023-08-03 21:08:09 -0700

    updated sql files, updated index page to be a general landing page

--- projstep2.sql
+++ projstep2.sql
@@ -1,21 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1-1.el7.remi
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jul 17, 2023 at 05:35 PM
--- Server version: 10.6.12-MariaDB-log
--- PHP Version: 8.2.4
+-- YNSRC Database DDL
+-- CS340 Project Group 110
+-- Alexander Dembo, Thomas Gullo
+-- Updated August 3rd, 2023
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `YNSRC`
@@ -28,19 +15,91 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `Customers` (
-  `CustomerID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `customerName` varchar(45) NOT NULL,
   `telephone` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+);
 
 --
--- Dumping data for table `Customers`
+-- Sample data for table `Customers`
 --
 
-INSERT INTO `Customers` (`CustomerID`, `customerName`, `telephone`) VALUES
-(1, 'Regina', '99-123-456-7890'),
-(2, 'George', '12-156-184-4452'),
-(3, 'Lee', '55-856-155-4154');
+INSERT INTO `Customers` (`customerName`, `telephone`) VALUES
+('Regina', '99-123-456-7890'),
+('George', '12-156-184-4452'),
+('Lee', '55-856-155-4154');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Spaceships`
+--
+
+CREATE TABLE `Spaceships` (
+  `spaceshipID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `customerID` int(11),
+  `spaceshipMake` varchar(45) NOT NULL,
+  `spaceshipModel` varchar(45) NOT NULL,
+  FOREIGN KEY (customerID) 
+	REFERENCES Customers(customerID) 
+	ON DELETE SET NULL
+);
+--
+-- Sample data for table `Spaceships`
+--
+
+INSERT INTO `Spaceships` (`customerID`, `spaceshipMake`, `spaceshipModel`) VALUES
+(3, 'Toyoda', 'Spamry'),
+(3, 'Jonda', 'Spinsight'),
+(2, 'Fjord', 'Fjocus'),
+(1, 'Starwagon', 'StarRabbit'),
+(1, 'Toyoda', 'Spamry');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Invoices`
+--
+
+CREATE TABLE `Invoices` (
+  `invoiceID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `cost` int(11) NOT NULL,
+  `spaceshipID` int(11) NOT NULL, 
+  FOREIGN KEY (spaceshipID) 
+	REFERENCES Spaceships(spaceshipID) 
+    ON DELETE RESTRICT
+);
+
+--
+-- Sample data for table `Invoices`
+--
+
+INSERT INTO `Invoices` (`cost`, `spaceshipID`) VALUES
+(85, 2),
+(95, 3),
+(30, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `RepairTypes`
+--
+
+CREATE TABLE `RepairTypes` (
+  `repairID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `repairName` varchar(45) NOT NULL,
+  `cost` int(11) NOT NULL
+);
+
+--
+-- Sample data for table `RepairTypes`
+--
+
+INSERT INTO `RepairTypes` (`repairName`, `cost`) VALUES
+('Galactic Oil Change', 35),
+('Spaceshield Replacement', 50),
+('Landing Gear Fluid Change', 45),
+('Muffler Repair', 30);
 
 -- --------------------------------------------------------
 
@@ -49,12 +108,14 @@ INSERT INTO `Customers` (`CustomerID`, `customerName`, `telephone`) VALUES
 --
 
 CREATE TABLE `InvoiceDetails` (
-  `invoiceID` int(11) NOT NULL,
-  `repairID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+  `invoiceID` int(11),
+  `repairID` int(11),
+  FOREIGN KEY (invoiceID) REFERENCES Invoices(invoiceID) ON DELETE CASCADE,
+  FOREIGN KEY (repairID) REFERENCES RepairTypes(repairID) ON DELETE CASCADE
+);
 
 --
--- Dumping data for table `InvoiceDetails`
+-- Sample data for table `InvoiceDetails`
 --
 
 INSERT INTO `InvoiceDetails` (`invoiceID`, `repairID`) VALUES
@@ -65,165 +126,3 @@ INSERT INTO `InvoiceDetails` (`invoiceID`, `repairID`) VALUES
 (3, 4);
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `Invoices`
---
-
-CREATE TABLE `Invoices` (
-  `invoiceID` int(11) NOT NULL,
-  `cost` int(11) NOT NULL,
-  `spaceshipID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
---
--- Dumping data for table `Invoices`
---
-
-INSERT INTO `Invoices` (`invoiceID`, `cost`, `spaceshipID`) VALUES
-(1, 85, 2),
-(2, 95, 3),
-(3, 30, 5);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `RepairTypes`
---
-
-CREATE TABLE `RepairTypes` (
-  `repairID` int(11) NOT NULL,
-  `repairName` varchar(45) NOT NULL,
-  `cost` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
---
--- Dumping data for table `RepairTypes`
---
-
-INSERT INTO `RepairTypes` (`repairID`, `repairName`, `cost`) VALUES
-(1, 'Galactic Oil Change', 35),
-(2, 'Spaceshield Replacement', 50),
-(3, 'Landing Gear Fluid Change', 45),
-(4, 'Muffler Repair', 30);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Spaceships`
---
-
-CREATE TABLE `Spaceships` (
-  `spaceshipID` int(11) NOT NULL,
-  `customerID` int(11),
-  `spaceshipMake` varchar(45) NOT NULL,
-  `spaceshipModel` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
---
--- Dumping data for table `Spaceships`
---
-
-INSERT INTO `Spaceships` (`spaceshipID`, `customerID`, `spaceshipMake`, `spaceshipModel`) VALUES
-(1, 3, 'Toyoda', 'Spamry'),
-(2, 3, 'Jonda', 'Spinsight'),
-(3, 2, 'Fjord', 'Fjocus'),
-(4, 1, 'Starwagon', 'StarRabbit'),
-(5, 1, 'Toyoda', 'Spamry');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Customers`
---
-ALTER TABLE `Customers`
-  ADD PRIMARY KEY (`CustomerID`);
-
---
--- Indexes for table `InvoiceDetails`
---
-ALTER TABLE `InvoiceDetails`
-  ADD KEY `invoiceID` (`invoiceID`),
-  ADD KEY `repairID` (`repairID`);
-
---
--- Indexes for table `Invoices`
---
-ALTER TABLE `Invoices`
-  ADD PRIMARY KEY (`invoiceID`),
-  ADD KEY `spaceshipID` (`spaceshipID`);
-
---
--- Indexes for table `RepairTypes`
---
-ALTER TABLE `RepairTypes`
-  ADD PRIMARY KEY (`repairID`);
-
---
--- Indexes for table `Spaceships`
---
-ALTER TABLE `Spaceships`
-  ADD PRIMARY KEY (`spaceshipID`),
-  ADD KEY `customerID` (`customerID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Customers`
---
-ALTER TABLE `Customers`
-  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `Invoices`
---
-ALTER TABLE `Invoices`
-  MODIFY `invoiceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `RepairTypes`
---
-ALTER TABLE `RepairTypes`
-  MODIFY `repairID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `Spaceships`
---
-ALTER TABLE `Spaceships`
-  MODIFY `spaceshipID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `InvoiceDetails`
---
-ALTER TABLE `InvoiceDetails`
-  ADD CONSTRAINT `InvoiceDetails_ibfk_1` FOREIGN KEY (`invoiceID`) REFERENCES `Invoices` (`invoiceID`)
-  ON DELETE CASCADE,
-  ADD CONSTRAINT `InvoiceDetails_ibfk_2` FOREIGN KEY (`repairID`) REFERENCES `RepairTypes` (`repairID`)
-  ON DELETE CASCADE;
-
---
--- Constraints for table `Invoices`
---
-ALTER TABLE `Invoices`
-  ADD CONSTRAINT `Invoices_ibfk_1` FOREIGN KEY (`spaceshipID`) REFERENCES `Spaceships` (`spaceshipID`)
-  ON DELETE NO ACTION;
-
---
--- Constraints for table `Spaceships`
---
-ALTER TABLE `Spaceships`
-  ADD CONSTRAINT `Spaceships_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `Customers` (`CustomerID`)
-  ON DELETE SET NULL;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- projstep3.sql
+++ projstep3.sql
@@ -1,3 +1,13 @@
+-- YNSRC Database DDL
+-- CS340 Project Group 110
+-- Alexander Dembo, Thomas Gullo
+-- Updated August 3rd, 2023
+
+
+--
+-- Database: `YNSRC`
+--
+
 -- Query for add a new character functionality with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
 
@@ -6,12 +16,21 @@ SELECT * FROM Customers WHERE :columnName = :input;
 INSERT INTO Customers (customerName, telephone)
 VALUES (:customerName, :telephone);
 
+-- Select a customer based off attribute values
+
 DELETE FROM Customers WHERE customerName = :customerName and telephone = :telephone;
+
+-- Select all spaceships where an attribute is equal to an input
 
 SELECT * FROM Spaceships WHERE :columnName = :input;
 
+-- Add a spaceship into the db
+
 INSERT INTO Spaceships (customerID, spaceshipMake, spaceshipModel)
 VALUES ((SELECT customerID FROM Customers WHERE customerName = :customerName), :spaceshipMake, :spaceshipModel);
+
+
+-- Select all invoices
 
 SELECT * FROM Invoices WHERE :columnName = :input;
 
@@ -32,12 +51,12 @@ INNER JOIN InvoiceDetails ON RepairTypes.repairID = repairID;
 
 -- update button is included above
 
---this is what happens when the "SAVE" from the "UPDATE" button is clicked
+-- this is what happens when the "SAVE" from the "UPDATE" button is clicked
 UPDATE Customers
 SET customerName = :customerName, telephone = :telephone
 WHERE customerID = :customerID;
 
---this is the delete button from RepairTypes. Gets rid of a whole row. 
+-- this is the delete button from RepairTypes. Gets rid of a whole row. 
 -- Note that since invoicedetails is delete on cascade, this will delete the associated row in the invoicedetails also
 DELETE FROM RepairTypes WHERE repairID = :repairID;
 
