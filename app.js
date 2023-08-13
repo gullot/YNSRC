8da42ab00ddafaa08dc2013ef1fa48a2d0194b8f
Thomas Gullo <gullot@flip1.engr.oregonstate.edu>
2023-08-12 17:17:43 -0700

    slight change on index

--- app.js
+++ app.js
@@ -25,13 +25,6 @@ app.get('/', function(req, res)
     {
 
     res.render('index');
-        //let query1 = "SELECT * FROM Customers";
-
-        //db.pool.query(query1, function(error, rows, fields){
-
-            //res.render('index', {data: rows});              // Note the call to render() and not send(). Using render() ensures the templating engine
-                                                // will process this file, before sending the finished HTML to the client.
-            //})
                           
     });    
 
