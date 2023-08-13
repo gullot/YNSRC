96d231be970f8fdc969d0cada4d1a9568c21ff5a
Thomas Gullo <gullot@flip1.engr.oregonstate.edu>
2023-08-12 18:46:49 -0700

    CSS and minor app.js change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();            // We need to instantiate an express object to i
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-PORT = 61287;                 // Set a port number at the top so it's easy to change in the future
+PORT = 61288;                 // Set a port number at the top so it's easy to change in the future
 
 // handlebars stuff
 const { engine } = require('express-handlebars');
@@ -23,12 +23,8 @@ var db = require('./database/db-connector')
 */
 app.get('/', function (req, res) {
 
-    db.pool.query(query1, function (error, rows, fields) {
-
-        res.render('index', { data: rows });              // Note the call to render() and not send(). Using render() ensures the templating engine
-        // will process this file, before sending the finished HTML to the client.
-    })
-
+        res.render('index');              // Note the call to render() and not send(). Using render() ensures the templating engine
+                                            // will process this file, before sending the finished HTML to the client.
 });
 
 // app.js - ROUTES section
