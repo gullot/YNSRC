c7b31e1b5dd88067171765d7e868508ff58495e5
Thomas Gullo <gullot@flip1.engr.oregonstate.edu>
2023-08-14 16:02:48 -0700

    forgot to remove username from dbcon as well as pword

--- database/db-connector.js
+++ database/db-connector.js
@@ -5,9 +5,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_gullot',
+    user            : 'cs340_xxxxx',
     password        : 'xxxx',
-    database        : 'cs340_gullot'
+    database        : 'cs340_xxxxx'
 });
 
 // Export it for use in our application
